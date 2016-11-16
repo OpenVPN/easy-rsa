6ba06da8da6d9fbcdedb4eb8c96ec4563ee67185
Samuli Seppänen <samuli@openvpn.net>
2016-11-16 17:14:21 +0200

    Include README.md in dist packages
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- build/build-dist.sh
+++ build/build-dist.sh
@@ -67,7 +67,7 @@ stage_unix() {
 	mkdir -p "$DIST_ROOT/unix/$PV"
 	
 	# Copy files into $PV, starting with easyrsa3 as the initial root dir
-	src_files="easyrsa3/ Licensing/ COPYING.md ChangeLog README.quickstart.md"
+	src_files="easyrsa3/ Licensing/ COPYING.md ChangeLog README.md README.quickstart.md"
 	for f in $src_files
 	do
 		cp -a "$SRC_ROOT/$f" "$DIST_ROOT/unix/$PV" || die "failed to copy $f"
@@ -92,7 +92,7 @@ stage_win() {
 		python -m markdown $f > $DIST_ROOT/windows/$PV/doc/$fname.html
 	done
 	
-	for f in "README.quickstart" "COPYING"
+	for f in "README" "README.quickstart" "COPYING"
 	do
 		python -m markdown $SRC_ROOT/$f.md > $DIST_ROOT/windows/$PV/$f.html
 	done
