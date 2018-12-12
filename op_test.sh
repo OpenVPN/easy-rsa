9db151e38835568d16ad55e90da8172d089428d8
Eric F Crist <ecrist@secure-computing.net>
2018-12-11 20:15:21 -0600

    Update op_test to support show-cert, add'l cmds
    Signed-off-by: Eric F Crist <ecrist@secure-computing.net>

--- op_test.sh
+++ op_test.sh
@@ -3,4 +3,13 @@
 # Runs some basic operational testing
 
 cd easyrsa3 || exit 1
+echo "===> Init-PKI"
 sh -x easyrsa init-pki
+echo "===> Build-CA"
+sh -x easyrsa -batch build-ca nopass
+echo "===> Build-Server"
+sh -x easyrsa -batch build-server full s01 nopass
+sh -x easyrsa show-cert s01
+echo "===> Build-Client"
+sh -x easyrsa -batch build-client-full c01 nopass
+sh -x easyrsa show-cert c01
