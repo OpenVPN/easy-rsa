c8a2f984528d6eb163117f3bc061c6a9b966dffc
Richard Bonhomme <tincanteksup@gmail.com>
2020-01-24 17:27:03 +0000

    op_test.sh: Correct exit code; Selectable unit-test source
    Signed-off-by: Richard Bonhomme <tincanteksup@gmail.com>

--- op_test.sh
+++ op_test.sh
@@ -4,11 +4,19 @@
 # and executes that - allows for disconnected testing from the easy-rsa
 # repo with TravisCI.
 
-curl -O 'https://raw.githubusercontent.com/OpenVPN/easyrsa-unit-tests/master/easyrsa-unit-tests.sh'
+# EG: Set in Travis: CUSTOM CONFIG
+# env: ERSA_UTEST_CURL_TARGET="tincantech/easyrsa-unit-tests"
+
+export ERSA_UTEST_CURL_TARGET="${ERSA_UTEST_CURL_TARGET:-OpenVPN/easyrsa-unit-tests}"
+
+curl -O "https://raw.githubusercontent.com/$ERSA_UTEST_CURL_TARGET/master/easyrsa-unit-tests.sh"
+EXIT_CODE=1
 
 if [ -e "easyrsa-unit-tests.sh" ];
 then
-	sh easyrsa-unit-tests.sh -v
+	EXIT_CODE=0
+	sh easyrsa-unit-tests.sh -vv || EXIT_CODE=1
+	rm easyrsa-unit-tests.sh
 fi
 
-rm easyrsa-unit-tests.sh
+exit "$EXIT_CODE"
