ffcdebc3183efb504d588a9840f58d1391c267da
Richard T Bonhomme <tincantech@protonmail.com>
2022-02-02 15:21:27 +0000

    Allow local unit-test script to remain after test completion
    Signed-off-by: Richard T Bonhomme <tincantech@protonmail.com>

--- op_test.sh
+++ op_test.sh
@@ -4,14 +4,19 @@
 # and executes that - allows for disconnected testing from the easy-rsa
 # repo with TravisCI.
 
-curl -O 'https://raw.githubusercontent.com/OpenVPN/easyrsa-unit-tests/master/easyrsa-unit-tests.sh'
+verb="${1:-'-v'}"
 
-if [ -e "easyrsa-unit-tests.sh" ];
-then
-	sh easyrsa-unit-tests.sh -v
+if [ ! -e "easyrsa-unit-tests.sh" ]; then
+	curl -O 'https://raw.githubusercontent.com/OpenVPN/easyrsa-unit-tests/master/easyrsa-unit-tests.sh'
+
+	if [ -e "easyrsa-unit-tests.sh" ]; then
+		sh easyrsa-unit-tests.sh "$verb"
+		estat=$?
+	fi
+	rm -f easyrsa-unit-tests.sh
+else
+	sh easyrsa-unit-tests.sh "$verb"
 	estat=$?
 fi
-
-rm -f easyrsa-unit-tests.sh
 
 exit $estat
