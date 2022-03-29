2b2ef5f7b8a1c2038124641a1e96d5eafdf20566
Richard T Bonhomme <tincantech@protonmail.com>
2022-03-29 14:00:45 +0100

    Enable shellcheck test - Use version 0.8.0 from easyrsa-unit-tests
    Signed-off-by: Richard T Bonhomme <tincantech@protonmail.com>

--- op_test.sh
+++ op_test.sh
@@ -4,6 +4,22 @@
 # and executes that - allows for disconnected testing from the easy-rsa
 # repo with TravisCI.
 
+github_url='https://raw.githubusercontent.com'
+
+if [ -e "shellcheck" ] && [ "$EASYRSA_NIX" ]; then
+	chmod +x shellcheck
+	./shellcheck -V
+	./shellcheck easyrsa3/easyrsa
+elif [ "$EASYRSA_NIX" ]; then
+	github_target='OpenVPN/easyrsa-unit-tests/master/shellcheck'
+	curl -O "${github_url}/${github_target}"
+	[ -e "shellcheck" ] || { echo "shellcheck download failed."; exit 9; }
+	chmod +x shellcheck
+	./shellcheck -V
+	./shellcheck easyrsa3/easyrsa
+	rm -f ./shellcheck
+fi
+
 case "$1" in
 -v)		verb='-v' ;;
 -vv)	verb='-vv' ;;
@@ -13,13 +29,20 @@ esac
 estat=0
 
 if [ -e "easyrsa-unit-tests.sh" ]; then
-	sh easyrsa-unit-tests.sh "$verb"
-	estat=$?
+	if sh easyrsa-unit-tests.sh "$verb"; then
+		: # ok
+	else
+		estat=1
+	fi
 else
-	curl -O 'https://raw.githubusercontent.com/OpenVPN/easyrsa-unit-tests/master/easyrsa-unit-tests.sh'
+	github_target='OpenVPN/easyrsa-unit-tests/master/easyrsa-unit-tests.sh'
+	curl -O "${github_url}/${github_target}"
 	[ -e "easyrsa-unit-tests.sh" ] || { echo "Unit-test download failed."; exit 9; }
-	sh easyrsa-unit-tests.sh "$verb"
-	estat=$?
+	if sh easyrsa-unit-tests.sh "$verb"; then
+		: # ok
+	else
+		estat=1
+	fi
 	rm -f easyrsa-unit-tests.sh
 fi
 
